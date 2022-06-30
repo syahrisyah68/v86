54b42375505ba94812e634ffe68ac0887b899ca9
nsrstory <76632542+syahrisyah68@users.noreply.github.com>
2022-06-30 07:41:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6287898307350'],
-  ['6287898307350'],
-  ['6287898307350', 'Owner', true]
+  ['6285893123964'],
+  ['6285893123964'],
+  ['6285893123964', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
